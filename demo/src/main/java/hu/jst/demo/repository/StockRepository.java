c4991cb9f4ab864d4e86e7a60e33317c9f2a47d0
Tamas Vajgely <vajgi90@gmail.com>
2019-12-31 15:53:39 +0100

    Corrected save and update database methods

--- demo/src/main/java/hu/jst/demo/repository/StockRepository.java
+++ demo/src/main/java/hu/jst/demo/repository/StockRepository.java
@@ -3,8 +3,12 @@ package hu.jst.demo.repository;
 import hu.jst.demo.entity.StockEntity;
 import hu.jst.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface StockRepository extends JpaRepository<StockEntity, Long> {
@@ -14,6 +18,24 @@ public interface StockRepository extends JpaRepository<StockEntity, Long> {
     @Query(value = "select * from stocks where symbol = ?1", nativeQuery = true)
     public StockEntity findBySymbol(String symbol);
 
-    @Query(value = "select ?1 from users where user_name = ?1", nativeQuery = true)
-    public String findBySymbol2(String symbol);
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE stocks SET " +
+            "change_ = ?2 ,change_percent = ?3,high = ?4,latest_trading_day = ?5, low = ?6, open_ = ?7, previous_close = ?8,price = ?9,volume = ?10 " +
+            "WHERE symbol = ?1", nativeQuery = true)
+    public void update(String symbol,
+                               Double change,
+                       String changePercent,
+                       Double high,
+                       LocalDate latestTradingDay,
+                       Double low,
+                       Double open,
+                       Double previousClose,
+                       Double price,
+                       Integer volume
+                              );
+
+    @Query(value = "SELECT EXISTS(SELECT * FROM stocks WHERE symbol = ?1)", nativeQuery = true)
+    public int isExist(String symbol);
+
 }
--- demo/src/main/java/hu/jst/demo/service/StockService.java
+++ demo/src/main/java/hu/jst/demo/service/StockService.java
@@ -36,10 +36,10 @@ public class StockService {
         return stockRepository.save(tesla);
     }
 
-    public StockEntity stockDownloader (String symbol) throws JsonProcessingException {
+    public StockEntity stockDownloader(String symbol) throws JsonProcessingException {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-        headers.set("x-rapidapi-key",  "03db22b5camshd34fd82b1dad7a3p13f99cjsn5141407b69d2");
+        headers.set("x-rapidapi-key", "03db22b5camshd34fd82b1dad7a3p13f99cjsn5141407b69d2");
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         String resourceUrl = "https://alpha-vantage.p.rapidapi.com/query?symbol=" + symbol.toUpperCase() + "&function=GLOBAL_QUOTE";
         HttpEntity<String> entity = new HttpEntity<String>(headers);
@@ -47,34 +47,33 @@ public class StockService {
         if (response.getStatusCode() == HttpStatus.OK) {
             JSONObject obj = new JSONObject(response.getBody().toString());
             obj = obj.getJSONObject("Global Quote");
-            StockEntity item = new StockEntity(
-                    obj.optString("01. symbol"),
-                    obj.optString("02. open"),
-                    obj.optString("03. high"),
-                    obj.optString("04. low"),
-                    obj.optString("05. price"),
-                    obj.optString("06. volume"),
-                    obj.optString("07. latest trading day"),
-                    obj.optString("08. previous close"),
-                    obj.optString("09. change"),
-                    obj.optString("10. change percent"));
-
-
-            stockRepository.save(item);
+            StockEntity item = jsonHandler(obj);
+            if (stockRepository.isExist(item.getSymbol()) == 0) {
+                stockRepository.save(item);
+            }
+            stockRepository.update(item.getSymbol(), item.getChange(), item.getChangePercent(),
+                    item.getHigh(), item.getLatestTradingDay(), item.getLow(), item.getOpen(), item.getPreviousClose(), item.getPrice(), item.getVolume());
             return item;
         }
         return null;
     }
 
-    public StockEntity saveStock(StockEntity item) {
-        if(stockRepository.findBySymbol2(item.getSymbol()).equals(""){
-            stockRepository.save(item);
-            return item;
-        }
-        stockRepository.delete(stockRepository.findBySymbol2(item.getSymbol());
-
-
+    public StockEntity jsonHandler(JSONObject obj) {
+        StockEntity item = new StockEntity(
+                obj.optString("01. symbol"),
+                obj.optString("02. open"),
+                obj.optString("03. high"),
+                obj.optString("04. low"),
+                obj.optString("05. price"),
+                obj.optString("06. volume"),
+                obj.optString("07. latest trading day"),
+                obj.optString("08. previous close"),
+                obj.optString("09. change"),
+                obj.optString("10. change percent"));
+        return item;
     }
 
-    }
+}
+
+
 
